a2332eb96813264334fd83a7235c961a86cc7c15
Mauro <mauro.filho@ua.pt>
2021-03-11 08:32:16 +0000

    Initial Commit

---
+++ usingCombine/usingCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  usingCombineApp.swift
+//  usingCombine
+//
+//  Created by Mauro Canhao on 11/03/21.
+//
+
+import SwiftUI
+
+@main
+struct usingCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
